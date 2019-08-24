32e4779ae00169bd7a405b06ada901eaf6d7bac3
initstring <26131150+initstring@users.noreply.github.com>
2019-08-24 11:34:09 +1000

    total rework

--- lyricpass.py
+++ lyricpass.py
@@ -1,125 +1,164 @@
+#!/usr/bin/env python3
+"""
+Utility to scrape lyrics from https://lyrics.wikia.com
+
+Used as a tool to generate raw input for the cleanup.py script in the password
+cracking project at github.com/initstring/passphrase-wordlist
+
+Usage:
+lyricspass.py -a <artist>
+lyricpass.py -i <file with multiple artists>
+
+Example:
+python lyricpass.py -a "Rob Zombie"
+python lyricpass.py -i /tmp/artists.txt
+
+Outputs two files:
+raw-lyrics.txt <everything>
+passphrases.txt <cleaned passphrases>
+"""
+
 import argparse
-from bs4 import BeautifulSoup
-import requests
+import urllib.request
+import os
 import sys
-import string
+import re
+
+SITE = "https://www.lyrics.com/"
 
 
-# Creating a class for the parser to gracefully handle errors:
-class MyParser(argparse.ArgumentParser):
-    def error(self, message):
-        sys.stderr.write('error: %s\n' % message)
-        self.print_help()
-        sys.exit(2)
+def parse_args():
+    """
+    Handle user-passed parameters
+    """
+    desc = "Scrape song lyrics from wikia.com"
+    parser = argparse.ArgumentParser(description=desc)
 
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument("-a", "--artist", type=str, action="store",
+                       help="Single artist to scrape")
+    group.add_argument("-i", "--infile", type=str, action="store",
+                       help="File containing one artist per line to scrape")
 
-# Handle the arguments before executing the main functions:
-parser = MyParser()
-parser.add_argument("artist", type=str, help="Define a specific artist for song lyric inclusion. Please  place \
-                     the artist name in quotes.", action="store")
-parser.add_argument("output", type=str, help="Output to file name in current directory.", action="store")
-parser.add_argument("--lower", help="Switches all letters to lower case.", action='store_true')
-parser.add_argument("--punctuation", help="Preserves punctuation, which is removed by default.", action='store_true')
-args = parser.parse_args()
+    args = parser.parse_args()
 
-artist = args.artist
-outfile = args.output
+    if args.infile:
+        if not os.access(args.infile, os.R_OK):                                     
+            print("[!] Cannot access input file, exiting")                          
+            sys.exit()
 
+    return args
 
-# The web site uses underscores in place of spaces. This function will format for us:
-def create_artist_url(a):
-    a = a.replace(' ', '_')
-    url = 'http://lyrics.wikia.com/wiki/' + a
-    return url
+def parse_artists(args):
+    """
+    Return a list of song artists for parsing
+    """
+    whitelist = re.compile('[^a-zA-Z0-9-+]')
+    artists = []
 
-# The site has a standard format for URLs. After we find the song names, we can use this to get the URL:
-def create_song_url(song, artist):
-    song = song.replace(' ', '_')
-    artist = artist.replace(' ', '_')
-    url = 'http://lyrics.wikia.com/wiki/' + artist + ':' + song
-    return url
+    if args.artist:
+        raw_artists = [args.artist,]
+    else:
+        with open(args.infile, encoding="utf-8", errors="ignore") as infile:
+            raw_artists = infile.readlines()
 
+    for artist in raw_artists:
+        artist = artist.replace(" ", "+")
+        artist = whitelist.sub("", artist)
+        if artist not in artists:
+            artists.append(artist)
 
-# This function attempts to create a list of links to songs based on the artist put in on the command line.
-# Later, we will use these song names to go looking for the lyrics:
-def get_songs(artisturl, artist):
-    cleanlinks = []
-    response = requests.get(artisturl)                       # We want to scrape the artist's landing page
-    soup = BeautifulSoup(response.content, "html.parser")
-    rawlinks = soup.select("ol li b a")                      # On that page, find the bulleted song lists
-    for l in rawlinks:
-        url = create_song_url(l.text, artist)                # Create a new link based on artist and song name
-        cleanlinks.append(url)                               # Stash this song link in a list to return
-    return cleanlinks
+    return artists
 
+def build_urls(artist):
+    """
+    Creates a list of song URLs for a specific artist
+    """
+    not_found = "We couldn't find any artists matching your query"
+    query_url = SITE + "/artist.php?name=" + artist
+    song_ids = []
+    regex = re.compile(r'href="/lyric/(.*?)/')
 
-# After we know the song names, we can use the artist name and the url function above to go find the actual lyrics.
-# This function does some basic cleaning of HTML tags out of the return strings. I found that unexpected data
-# is occasionally returned and generated errors trying to append to a list, so we will work around that with try
-# and except:
-def get_lyrics(songurl):
-    l = []
-    response = requests.get(songurl)                            # Now we scrape each individual song page
-    soup = BeautifulSoup(response.content, "html.parser")       # Use bs4 to parse the html data
-    lyricbox = soup.find('div', {'class': 'lyricbox'})          # The lyrics are stored in a div tag called lyricbox
-    if lyricbox:                                                # Verify we find lyrics on the page
-        for line in lyricbox:
-            line = line.encode('utf-8').strip()                  # Encode the data and remove whitespaces
-            if line and '<' not in line.decode('utf-8') and '\\' not in line.decode('utf-8'):   # Hacky, need to improve
-                try:
-                    l.append(line)                              # If this is good, clean text: append it to the list
-                except:
-                    continue
-    print ("Found " + str(len(l)) + " lines of lyrics")
-    return l                                                    # This returns a long list of lyrics to the main func
+    with urllib.request.urlopen(query_url) as response:
+        html = response.read().decode()
 
+    song_ids = re.findall(regex, html)   
 
-# This function can be used to further deduplicate the list after punctuation is removed.
-def dedupe(seq):
-    seen = set()
-    seen_add = seen.add
-    return [x for x in seq if not (x in seen or seen_add(x))]
+    if not_found in html:
+        print("[!] Artist {} not found, skipping".format(artist))
 
+        # Clear out the "suggested" songs it finds in this scenario
+        song_ids = []
+    elif not song_ids:
+        print("[!] No songs found for {}, skipping".format(artist))
+    else:
+        print("[+] Found {} songs for artists {}"
+              .format(len(song_ids), artist))
 
-# This function cleans up the data before writing, including any optional parameters specified at launch:
-def format_lyrics(rawlyrics):
-    formatted = rawlyrics
-    if args.lower:
-        formatted = [element.lower() for element in formatted]
-    if not args.punctuation:
-        formatted = [''.join(c for c in s if c not in string.punctuation) for s in formatted]
-    formatted = dedupe(formatted)
+    url_list = [SITE + "print.php?id=" + id for id in song_ids]
+    return url_list
 
-    return formatted
+def write_data(outfile, data):
+    """
+    Generic helper function to write text to a file
+    """
+    with open(outfile, "a") as open_file:
+        for line in data:
+            if line:
+                open_file.write(line + '\n')
 
+def scrape_lyrics(url_list):
+    """
+    Scrapes raw lyric data from a list of URLs
+    """
+    regex = re.compile(r"<pre.*?>(.*?)</pre>", re.DOTALL)
+    newline = re.compile(r"\r\n|\n")
 
-# This function will append the found lyrics to a file in the current directory:
-def write_file(l, o):
-    file = open(o, 'a')
-    for line in l:
-        try:
-            file.write(str(line) + '\n')
-        except:
-            continue
+    deduped_lyrics = set()
+
+    current = 1
+    total = len(url_list)
+
+    for url in url_list:
+        print("Checking song {}/{}...       \r".format(current, total), end="")
+
+        with urllib.request.urlopen(url) as response:
+            html = response.read().decode()
+
+        lyrics = re.findall(regex, html)[0]
+        lyrics = re.split(newline, lyrics)
+
+        write_data('raw-lyrics.txt', lyrics)
+
+        deduped_lyrics.update(lyrics)
+
+        current += 1
+
+    return deduped_lyrics
+            
 
 
 def main():
-    lyrics = []
-    print('Looking for lyrics from ' + artist + ' and writing to file: ' + outfile)
+    """
+    Main program function
+    """
+    args = parse_args()
+    artists = parse_artists(args)
 
-    artisturl = create_artist_url(artist)               # create a workable URL
-    songlinks = get_songs(artisturl, artist)            # find all the songs for this artist
-    for s in songlinks:
-        print("Getting lyrics for " + s)
-        for l in get_lyrics(s):                         # get a list of lyric lines
-            try:
-                lyrics.append(l)                        # append found lines to master list
-            except:
-                continue
-    lyrics = format_lyrics(lyrics)                      # format lyrics as specified in arguments
-    print("*********************")
-    print("Now writing output file...")
-    write_file(lyrics, outfile)                         # write the output file
+    raw_words = set()
+
+    for artist in artists:
+        print("[+] Looking up artist {}".format(artist))
+        url_list = build_urls(artist)
+        if not url_list:
+            continue
+        raw_words.update(scrape_lyrics(url_list))
+
+    # TO DO: Implement cleaning function before writing this one.
+    write_data("wordlist.txt", raw_words)
+
+    print("[+] All done! Check out raw-lyrics.txt and wordlist.txt in your"
+          " current directory.")
 
 
 
